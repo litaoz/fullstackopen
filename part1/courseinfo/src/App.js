6b5e6d463559d24afc4aaecc05a4579125c3df92
litaoz <litaored@gmail.com>
2021-04-02 11:15:08 -0400

    1.3 refactor to use objects

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Header = (props) => {
+  /* props.course */
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1}/>
+      <Part part={props.part2}/>
+      <Part part={props.part3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+}
+
+const Total = (props) => {
+    return (
+      <p>Number of exercises {props.exercises}</p>
+    )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1}
+               part2={part2}
+               part3={part3}/>
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
+    </div>
+  )
+}
+
+export default App
