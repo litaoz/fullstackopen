76acf80e8205737df0302ec98e82f8e47521e45c
litaoz <litaored@gmail.com>
2021-04-05 13:43:13 -0400

    unicafe basic layout

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h2>give feedback</h2>
+        <button>good</button>
+        <button>neutral</button>
+        <button>bad</button>
+      </div>
+      <div>
+        <h2>statistics</h2>
+        <p>good 6</p>
+        <p>neutral 2</p>
+        <p>bad 1</p>
+      </div>
     </div>
   );
 }
