f6e481c961f62896ba1c4f5570eb0eb3720a062f
litaoz <litaored@gmail.com>
2021-05-30 17:21:40 -0400

    Added controller

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,30 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const blogsRouter = require('./controllers/blogs')
-
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
-// app.use('/api/blogs', blogsRouter)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
